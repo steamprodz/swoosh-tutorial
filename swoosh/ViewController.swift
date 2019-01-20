3bd06625cbd8184ca935a55899fdcb3a84f1a120
Andrii Diachenko <andreydmg@gmail.com>
2019-01-20 14:03:39 +0200

    Initial Commit

---
+++ swoosh/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  swoosh
+//
+//  Created by Andrii Diachenko on 1/20/19.
+//  Copyright © 2019 Andrii Diachenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
